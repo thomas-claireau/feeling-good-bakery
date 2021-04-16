c5cf65702f67807e6e09ee4acc85aa3ebcff660d
Thomas Claireau <thomas.claireau@gmail.com>
2021-04-16 15:33:55 +0200

    main js remove useless code

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
 import "../scss/style.scss";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+console.log("passe");
