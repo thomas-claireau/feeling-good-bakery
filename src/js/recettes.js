a2db83e74e8df89fd5cff4781875c6f7f6ceea3b
Thomas Claireau <thomas.claireau@gmail.com>
2021-04-23 14:56:34 +0200

    recettes js -> generate modals OK

--- src/js/recettes.js
+++ src/js/recettes.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const swiperContainer = recettesContainer.querySelector(".swiper-container");
   const swiperPrev = swiperContainer.querySelector(".swiper-prev");
   const swiperNext = swiperContainer.querySelector(".swiper-next");
-  const readMore = document.querySelector("a.read-more");
+  const readMore = document.querySelector("button.read-more");
 
   const swiper = new Swiper(swiperContainer, {
     loop: true,
@@ -22,6 +22,30 @@ document.addEventListener("DOMContentLoaded", () => {
   swiper.on("slideChangeTransitionEnd", function () {
     const slide = swiper.slides[swiper.activeIndex];
 
-    readMore.href = `#${slide.dataset.id}`;
+    readMore.dataset.id = `${slide.dataset.id}`;
   });
+
+  // generate modals
+  const modals = document.querySelector("section.modals");
+
+  fetch("../../assets/recettes.json")
+    .then((res) => res.json())
+    .then((data) => {
+      Object.entries(data).forEach((cookie) => {
+        modals.innerHTML += generateModal(cookie[0], cookie[1]);
+      });
+    });
+
+  function generateModal(index, data) {
+    return `<div class="modal">
+			<img src="./assets/img/icons/cross-modal.png" alt="close-modal" class="close" />
+			<div class="content">
+				<img src="./assets/img//recettes/${index}.png" alt="cookie-${index}" />
+				<h2>${data.title}</h2>
+				<p>
+					${data.description}
+				</p>
+			</div>
+	  	</div>`;
+  }
 });
