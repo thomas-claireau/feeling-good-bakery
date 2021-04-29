0e7f494c50a7f01b0692b66fa0b35ec8b85ed83a
Thomas Claireau <thomas.claireau@gmail.com>
2021-04-29 15:50:34 +0200

    wip footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -6,4 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const date = footer.querySelector(".date");
   const now = new Date();
   date.textContent = now.getFullYear();
+
+  document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+      const switchs = footer.querySelectorAll(".switch");
+
+      switchs.forEach((item) => {
+        const firstSpan = item.querySelector("span:first-child");
+        const width = firstSpan.getBoundingClientRect().width;
+
+        console.log(width);
+      });
+    }
+  };
 });
