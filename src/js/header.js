54f0c2a19bc3ef78123c618f9f4e683db68f7055
Thomas Claireau <thomas.claireau@gmail.com>
2021-04-30 17:51:38 +0200

    add interaction js menu

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   const burgerMenu = document.querySelector("header .burger-menu");
+
+  if (!burgerMenu) return;
+
   const handle = burgerMenu.querySelector(".handle");
+  const hero = document.querySelector("section.hero");
+  const navigation = burgerMenu.querySelector('ul[role="navigation"]');
 
   handle.addEventListener("click", () => {
     handle.classList.toggle("open");
+    navigation.classList.toggle("hidden");
   });
+
+  addNavigationHeight();
+
+  window.addEventListener("resize", addNavigationHeight);
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      handle.classList.remove("open");
+      navigation.classList.add("hidden");
+    }
+  });
+
+  function addNavigationHeight() {
+    const bottom = Math.floor(hero.getBoundingClientRect().bottom);
+
+    navigation.style.height = bottom + "px";
+  }
 });
